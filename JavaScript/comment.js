bb3fb76612ca920852eb60152947c2d588c3578b
zygame1314 <zygame1314@gmail.com>
2025-07-15 15:44:23 +0800

    更新表情源 URL，修正为正确的存储路径

--- JavaScript/comment.js
+++ JavaScript/comment.js
@@ -13,8 +13,8 @@ init({
     highlight: true,
     recordIP: true,
     emoji: [
-        'https://bucket.zygame1314.top/static/emoji/bilibili_tv',
-        'https://bucket.zygame1314.top/static/emoji/blobs-gif'
+        'https://bucket.zygame1314.site/static/emoji/bilibili_tv',
+        'https://bucket.zygame1314.site/static/emoji/blobs-gif'
     ],
     imageUploader: async (file) => {
         const webpBlob = await convertToWebP(file);
